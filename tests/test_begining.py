ed5c79ffc79d453331c765b9e6101865bdf4fd11
Vito <farinavito@gmail.com>
2022-02-09 23:12:12 +0100

    fixed test_timeNotBreached_breached_on_time_false_send_deposit

--- tests/test_begining.py
+++ tests/test_begining.py
@@ -931,6 +931,7 @@ def test_timeNotBreached_breached_on_time_false_send_deposit(deploy, seconds_sle
     chain = Chain()
     chain.sleep(seconds_sleep)
     deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': 4*amount_sent}) 
+    deploy.withdraw_when_you_are_receiver(agreements_number, {'from': accounts[receiver]})
     assert accounts[receiver].balance() == balance_receiver + amount_sent
 
 @pytest.mark.parametrize("seconds_sleep",  [0, less_than_every_period[0], less_than_every_period[1], less_than_every_period[2]])
