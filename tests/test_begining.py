77fd50b706e452b25e3a326c45e413914a5f8df2
Vito <farinavito@gmail.com>
2022-02-22 00:51:26 +0100

    created test_withdrawAsTheSignee_withdrawal_sent

--- tests/test_begining.py
+++ tests/test_begining.py
@@ -1319,6 +1319,19 @@ def test_withdrawAsTheSignee_second_reguire_fails(deploy, time):
     with brownie.reverts("There aren't any funds to withdraw"):
         deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
 
+@pytest.mark.parametrize("time", [more_than_agreement_duration[0], more_than_agreement_duration[1], more_than_agreement_duration[2]])
+def test_withdrawAsTheSignee_withdrawal_sent(deploy, time):
+    '''Check if the withdrawal is sent'''
+    deploy.confirmAgreement(agreements_number, {'from': accounts[receiver]})
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': 4*amount_sent})
+    signee_balance = accounts[signee].balance()
+    chain = Chain()
+    chain.sleep(time)
+    deploy.terminateContract(agreements_number, {'from': accounts[signee]})
+    deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
+    assert accounts[signee].balance() == signee_balance + amount_sent
+
 
 
 '''TEST GETWITHDRAWALRECEIVER'''
