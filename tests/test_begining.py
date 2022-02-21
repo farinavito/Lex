fc7fe3a16f56e5855af433f215513ce39592621a
Vito <farinavito@gmail.com>
2022-02-22 00:42:06 +0100

    created test_withdrawAsTheReceiver_withdrawal_sent

--- tests/test_begining.py
+++ tests/test_begining.py
@@ -1260,6 +1260,15 @@ def test_withdrawAsTheReceiver_second_reguire_fails_case_2(deploy):
     with brownie.reverts("There aren't any funds to withdraw"):
         deploy.withdrawAsTheReceiver(agreements_number, {'from': accounts[receiver]})
 
+def test_withdrawAsTheReceiver_withdrawal_sent(deploy):
+    '''Check if the withdrawal was sent to receiver'''
+    deploy.confirmAgreement(agreements_number, {'from': accounts[receiver]})
+    receiver_balance = accounts[receiver].balance()
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
+    deploy.withdrawAsTheReceiver(agreements_number, {'from': accounts[receiver]})
+    assert accounts[receiver].balance() == receiver_balance + amount_sent - commission
+
 
 
 '''TEST WITHDRAWASTHESIGNEE'''
