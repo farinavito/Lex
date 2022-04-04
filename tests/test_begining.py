c95272333c4edbf06508fbf783d691fd1a7510b5
Vito <farinavito@gmail.com>
2022-04-04 09:58:07 +0200

    fixed test_withdrawAsTheSignee_second_reguire_fails

--- tests/test_begining.py
+++ tests/test_begining.py
@@ -955,17 +955,16 @@ def test_withdrawAsTheSignee_first_reguire_fails_pair(deploy, time):
     chain.sleep(time)
     function_initialize = deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
     assert function_initialize.events[0][0]['message'] == "Withdrawal has been transfered"
-'''
+@pytest.mark.aaa
 @pytest.mark.parametrize("time", [more_than_agreement_duration[0], more_than_agreement_duration[1], more_than_agreement_duration[2]])
 def test_withdrawAsTheSignee_second_reguire_fails(deploy, time):
-    require statement withdraw_receiver[exactAgreement[_id].signee] > 0 fails, because we already withdraw the funds
+    '''require statement withdraw_receiver[exactAgreement[_id].signee] > 0 fails, because we already withdraw the funds'''
     deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
     chain = Chain()
     chain.sleep(time)
-    deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
     with brownie.reverts("There aren't any funds to withdraw"):
         deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
-'''
+
 '''
 @pytest.mark.parametrize("time", [more_than_agreement_duration[0], more_than_agreement_duration[1], more_than_agreement_duration[2]])
 def test_withdrawAsTheSignee_withdrawal_sent(deploy, time):
