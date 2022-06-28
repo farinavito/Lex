19a0aef7c101e2bf64c9f8b01afed4b806edd2b5
vito <farinavito@gmail.com>
2022-06-28 08:52:48 +0200

    removed commented code

--- contracts/sc.sol
+++ contracts/sc.sol
@@ -276,8 +276,6 @@ contract AgreementBetweenSubjects {
       if (timeWasntBreached(_id)){
         emit NotifyUser("The agreement wasn't breached");
       } else {
-        //receiver has to wait 7 days after the breached date to withdraw the deposit -> doesn't work
-        //require(exactAgreement[_id].positionPeriod + (60*60*24*7) < block.timestamp, "You can't withdraw the deposit before 7 days after breached deadline");
         //terminate the agreement
         exactAgreement[_id].status = "Terminated";
         //return deposit to receiver
@@ -292,8 +290,6 @@ contract AgreementBetweenSubjects {
       if (exactAgreement[_id].agreementStartDate + (6*60*60*24) > block.timestamp){
         emit NotifyUser("The agreement hasn't been breached");
       } else {
-        //receiver has to wait 7 days after the breached date to withdraw the deposit -> doesn't work
-        //require(exactAgreement[_id].positionPeriod + (60*60*24*7) < block.timestamp, "You can't withdraw the deposit before 7 days after breached deadline");
         //terminate the agreement
         exactAgreement[_id].status = "Terminated";
         //return deposit to receiver
